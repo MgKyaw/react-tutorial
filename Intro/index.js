4d3ad6776f0234258b2ba7b16784bdaf985aa8ba
MgKyaw <mgkyaw20112@gmail.com>
2025-02-16 20:11:16 +0700

    feat: Add array of objects filter

--- Intro/index.js
+++ Intro/index.js
@@ -22,3 +22,25 @@
 
 // // sample usage (do not modify)
 // console.log(getPositiveNumbers([10, -5, 2, -4]));
+
+// Array of objects filter
+/**
+ * @param {object[]} results
+ */
+function getPassingTests(results) {
+    console.log(results);
+
+}
+
+// sample usage (do not modify)
+const data = [{
+    id: 1,
+    grade: 10
+}, {
+    id: 2,
+    grade: 4
+}, {
+    id: 3,
+    grade: 18
+}]
+console.log(getPassingTests(data))
