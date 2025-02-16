ecae337991bd3783818d1f31d945cb5b9afdd6cc
MgKyaw <mgkyaw20112@gmail.com>
2025-02-16 20:10:39 +0700

    feat: Comment array tutorial

--- Intro/index.js
+++ Intro/index.js
@@ -11,15 +11,14 @@
 // console.log(sayLouder("It works"))
 // console.log(sayLouder("how are you?"));
 
+// // Array
+// /**
+//  * @param {number[]} numbers
+//  */
+// function getPositiveNumbers(numbers) {
+//     console.log(numbers);
+//     return numbers.filter(number => number > 0);
+// }
 
-// Array
-/**
- * @param {number[]} numbers
- */
-function getPositiveNumbers(numbers) {
-    console.log(numbers);
-    return numbers.filter(number => number > 0);
-}
-
-// sample usage (do not modify)
-console.log(getPositiveNumbers([10, -5, 2, -4]));
+// // sample usage (do not modify)
+// console.log(getPositiveNumbers([10, -5, 2, -4]));
