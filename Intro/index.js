a119766bfeec22894be426ae3eaafdb48fd482d1
MgKyaw <mgkyaw20112@gmail.com>
2025-02-16 20:03:09 +0700

    feat: Add sayLouder method

---
+++ Intro/index.js
@@ -0,0 +1,12 @@
+/**
+ * 
+ * @param {*} text 
+ * @returns 
+ */
+function sayLouder(text) {
+
+}
+
+// sample usage (do not modify)
+console.log(sayLouder("It works"))
+console.log(sayLouder("how are you?"));
