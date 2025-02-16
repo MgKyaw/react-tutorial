de0eea95ed8b12b0e2fe179f313f15d0320a8a54
MgKyaw <mgkyaw20112@gmail.com>
2025-02-16 20:09:21 +0700

    feat: Add Array tutorial

--- Intro/index.js
+++ Intro/index.js
@@ -10,3 +10,16 @@
 // // sample usage (do not modify)
 // console.log(sayLouder("It works"))
 // console.log(sayLouder("how are you?"));
+
+
+// Array
+/**
+ * @param {number[]} numbers
+ */
+function getPositiveNumbers(numbers) {
+    console.log(numbers);
+
+}
+
+// sample usage (do not modify)
+console.log(getPositiveNumbers([10, -5, 2, -4]));
