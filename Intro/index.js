3a89945ed48a355d59cdca67fa7dfb1be72468ea
MgKyaw <mgkyaw20112@gmail.com>
2025-02-19 18:26:46 +0700

    feat: Add code for notification count

--- Intro/index.js
+++ Intro/index.js
@@ -136,3 +136,18 @@
 //     age: 34
 // });
 // console.log(element);
+
+function getNotifications(notifications) {
+
+}
+
+// sample usage (do not modify)
+const notifications = [{
+    id: 1,
+    text: "Your order has been delivered"
+}, {
+    id: 2,
+    text: "Your coupon code has expired"
+}];
+const element = getNotifications(notifications);
+console.log(element);
