3d250d52bb2654ab94f925dfa9f065d29ffd7447
MgKyaw <mgkyaw20112@gmail.com>
2025-02-19 18:31:24 +0700

    feat: Comment showing noti count

--- Intro/index.js
+++ Intro/index.js
@@ -137,17 +137,17 @@
 // });
 // console.log(element);
 
-function getNotifications(notifications) {
-    return <p>You have {notifications.length} new notifications</p>;
-}
+// function getNotifications(notifications) {
+//     return <p>You have {notifications.length} new notifications</p>;
+// }
 
-// sample usage (do not modify)
-const notifications = [{
-    id: 1,
-    text: "Your order has been delivered"
-}, {
-    id: 2,
-    text: "Your coupon code has expired"
-}];
-const element = getNotifications(notifications);
-console.log(element);
+// // sample usage (do not modify)
+// const notifications = [{
+//     id: 1,
+//     text: "Your order has been delivered"
+// }, {
+//     id: 2,
+//     text: "Your coupon code has expired"
+// }];
+// const element = getNotifications(notifications);
+// console.log(element);
