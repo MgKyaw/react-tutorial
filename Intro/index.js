e9e96f59e89d817030c2e440a63a624685e9354e
MgKyaw <mgkyaw20112@gmail.com>
2025-02-20 22:19:49 +0700

    feat: Return footer

--- Intro/index.js
+++ Intro/index.js
@@ -219,7 +219,15 @@
 // console.log(getUserAvatar(url));
 
 function getFooter() {
-
+    return <>
+        <h3>Your online supermarket</h3>
+        <ul>
+            <li>Contact</li>
+            <li>Press</li>
+            <li>About</li>
+        </ul>
+        <p>All rights reserved @copy;</p>
+    </>;
 }
 
 // sample usage (do not modify)
