65840f4c485574728bc51a0ad15bf2ab98afa399
MgKyaw <mgkyaw20112@gmail.com>
2025-02-20 21:38:39 +0700

    feat: Return select with 3 children

--- Intro/index.js
+++ Intro/index.js
@@ -188,7 +188,11 @@
 // console.log(getPaymentMethod("Paypal"));
 
 function getPaymentMethods() {
-
+    return <select className="payment-methods">
+        <option className="payment-method">Card</option>
+        <option className="payment-method">Paypal</option>
+        <option className="payment-method">Cash on delivery</option>
+    </select>;
 }
 
 // sample usage (do not modify)
