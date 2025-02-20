87d082d7ec15a949c81899652b2098de96daeeb1
MgKyaw <mgkyaw20112@gmail.com>
2025-02-20 21:44:30 +0700

    feat: Comment dropdown menu

--- Intro/index.js
+++ Intro/index.js
@@ -198,14 +198,14 @@
 // // sample usage (do not modify)
 // console.log(getPaymentMethods());
 
-function getDropdownMenu() {
-    return <ul id="dropdown-menu" className="dropdown">
-        <li>Profile</li>
-        <li>Orders</li>
-        <li className="separator"></li>
-        <li>Logout</li>
-    </ul>;
-}
+// function getDropdownMenu() {
+//     return <ul id="dropdown-menu" className="dropdown">
+//         <li>Profile</li>
+//         <li>Orders</li>
+//         <li className="separator"></li>
+//         <li>Logout</li>
+//     </ul>;
+// }
 
-// sample usage (do not modify)
-console.log(getDropdownMenu());
+// // sample usage (do not modify)
+// console.log(getDropdownMenu());
