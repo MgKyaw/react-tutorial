0e577ae875ef6a3ef859aceeb8d79c25f2bbf2fe
MgKyaw <mgkyaw20112@gmail.com>
2025-02-21 21:47:30 +0700

    feat: Add Hero component

--- Intro/index.js
+++ Intro/index.js
@@ -235,6 +235,14 @@
 
 import {createRoot} from "react-dom/client";
 
+function Hero() {
+    return (
+        <>
+            <h1>Welcome to our supermarket</h1>
+            <p>Start shopping.</p>
+        </>
+    );
+}
 
 // do not modify
 createRoot(document.querySelector("#react-root")).render(<Hero></Hero>);
