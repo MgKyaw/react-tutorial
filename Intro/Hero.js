49311552fa04741eee63a634a04fd47ddad80246
MgKyaw <mgkyaw20112@gmail.com>
2025-02-21 22:06:20 +0700

    feat: Add Hero.js component file

---
+++ Intro/Hero.js
@@ -0,0 +1,9 @@
+// Define the Hero Component
+export default function Hero() {
+    return (
+        <>
+            <h1 className="title">Supermarket</h1>
+            <p>Start shopping</p>
+        </>
+    );
+}
