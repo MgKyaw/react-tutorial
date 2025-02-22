7c62c7f6e6ca95fd29d630a0db4f216cfeb1dced
MgKyaw <mgkyaw20112@gmail.com>
2025-02-22 22:50:24 +0700

    feat: Comment displaying props values

--- Intro/index.js
+++ Intro/index.js
@@ -297,30 +297,30 @@
 // const root = document.querySelector("#react-root");
 // createRoot(root).render(<ShoppingCart count="5" />);
 
-import {createRoot} from "react-dom/client";
+// import {createRoot} from "react-dom/client";
 
-function Navbar(props) {
-    return (
-        <>
-            <div>Welcome {props.user.first_name} {props.user.last_name}</div>
-            <p>You've got {props.notifications.length} notifications</p>
-        </>
-    );
-}
+// function Navbar(props) {
+//     return (
+//         <>
+//             <div>Welcome {props.user.first_name} {props.user.last_name}</div>
+//             <p>You've got {props.notifications.length} notifications</p>
+//         </>
+//     );
+// }
 
-let notifications = [{
-    id: 1,
-    text: "Order delivered"
-}, {
-    id: 2,
-    text: "Order received"
-}];
+// let notifications = [{
+//     id: 1,
+//     text: "Order delivered"
+// }, {
+//     id: 2,
+//     text: "Order received"
+// }];
 
-const user = {
-    first_name: "Sam",
-    last_name: "Wolf"
-};
+// const user = {
+//     first_name: "Sam",
+//     last_name: "Wolf"
+// };
 
-const root = document.querySelector("#react-root");
-createRoot(root).render(<Navbar notifications={notifications} user={user} />);
+// const root = document.querySelector("#react-root");
+// createRoot(root).render(<Navbar notifications={notifications} user={user} />);
 
