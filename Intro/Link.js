166d6fb5bfbf259d49e8c1c1cefe70b3da7f0377
MgKyaw <mgkyaw20112@gmail.com>
2025-02-23 21:30:32 +0700

    feat: Add Link component code

--- Intro/Link.js
+++ Intro/Link.js
@@ -1,3 +1,8 @@
 // export default function Link() {
 //     return <a>Link text</a>;
 // }
+
+export default function Link({ href, children}) {
+    const className = "ui-link";
+    return <a href={href} className={className}>{children}</a>;
+}
