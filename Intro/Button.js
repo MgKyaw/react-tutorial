8bae40b5d7ae99caf6d456624b4cdfc7de38c176
MgKyaw <mgkyaw20112@gmail.com>
2025-02-23 21:32:36 +0700

    feat: Add Button component code

--- Intro/Button.js
+++ Intro/Button.js
@@ -1,3 +1,8 @@
 // export default function Button() {
 //     return <button>Button text</button>;
 // }
+
+export default function Button({ type, disabled, children}) {
+    const className = "ui-button";
+    return <button type={type} disabled={disabled} className={className}>{children}</button>;
+}
