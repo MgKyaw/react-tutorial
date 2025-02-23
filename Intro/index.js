b58c514c41f9047b80cedf55f26b52cecb43340f
MgKyaw <mgkyaw20112@gmail.com>
2025-02-23 21:00:30 +0700

    feat: Comment props.children

--- Intro/index.js
+++ Intro/index.js
@@ -339,12 +339,12 @@
 // const root = document.querySelector("#react-root");
 // createRoot(root).render(<Button size="small" />);
 
-import {createRoot} from "react-dom/client";
+// import {createRoot} from "react-dom/client";
 
-function Button(props){
-    return <button>{props.children}</button>;
-}
+// function Button(props){
+//     return <button>{props.children}</button>;
+// }
 
-const root = document.querySelector("#react-root");
+// const root = document.querySelector("#react-root");
 
-createRoot(root).render(<Button>Login</Button>);
+// createRoot(root).render(<Button>Login</Button>);
